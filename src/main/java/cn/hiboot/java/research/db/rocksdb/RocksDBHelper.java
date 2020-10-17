5da82137aca7193c5b5c6a9f02153bcc8c2c8666
DingHao <dh.hiekn@gmail.com>
2020-10-17 23:11:57 +0800

    RocksDBHelper

---
+++ src/main/java/cn/hiboot/java/research/db/rocksdb/RocksDBHelper.java
@@ -0,0 +1,68 @@
+package cn.hiboot.java.research.db.rocksdb;
+
+import org.rocksdb.*;
+import org.rocksdb.util.ByteUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * description about this class
+ *
+ * @author DingHao
+ * @since 2020/10/17 21:41
+ */
+public class RocksDBHelper {
+
+    public static void execute(String dbPath, Consumer<RocksDB> consumer) throws RocksDBException {
+        try (final Options options = new Options().setCreateIfMissing(true)) {
+            mkdir(dbPath);
+            try (final RocksDB rocksDB = RocksDB.open(options, dbPath)) {
+                consumer.accept(rocksDB);
+            }
+        }
+    }
+
+    private static boolean mkdir(String path){
+        File file = new File(path);
+        if(file.exists()){
+            return false;
+        }
+        return file.mkdirs();
+    }
+
+    public static void execute(String dbPath, String cfName,BiConsumer<RocksDB,List<ColumnFamilyHandle>> consumer) throws RocksDBException {
+        try (final ColumnFamilyOptions cfOpts = new ColumnFamilyOptions().optimizeUniversalStyleCompaction()) {
+            try (final DBOptions options = new DBOptions().setCreateIfMissing(true).setCreateMissingColumnFamilies(true)) {
+                mkdir(dbPath);
+                // list of column family descriptors, first entry must always be default column family
+                final List<ColumnFamilyDescriptor> cfDescriptors = Arrays.asList(
+                        new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY, cfOpts),
+                        new ColumnFamilyDescriptor(cfName.getBytes(), cfOpts)
+                );
+                List<ColumnFamilyHandle> cfHandles = new ArrayList<>();
+                try (final RocksDB rocksDB = RocksDB.open(options, dbPath, cfDescriptors, cfHandles)) {
+                    consumer.accept(rocksDB,cfHandles);
+                }finally {
+                    // NOTE frees the column family handles before freeing the db
+                    for (final ColumnFamilyHandle cfHandle : cfHandles) {
+                        cfHandle.close();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void put(RocksDB rocksDB,String key,Object value) throws RocksDBException {
+        rocksDB.put(ByteUtil.bytes(key), ByteUtil.bytes(value.toString()));
+    }
+
+    public static byte[] get(RocksDB rocksDB,String key) throws RocksDBException {
+       return rocksDB.get(ByteUtil.bytes(key));
+    }
+
+}
--- src/main/java/cn/hiboot/java/research/db/rocksdb/RocksDemo.java
+++ src/main/java/cn/hiboot/java/research/db/rocksdb/RocksDemo.java
@@ -1,10 +1,13 @@
 package cn.hiboot.java.research.db.rocksdb;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.rocksdb.Options;
-import org.rocksdb.RocksDB;
+import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * describe about this class
@@ -13,20 +16,111 @@ import org.rocksdb.RocksDBException;
  * @since 2020/3/24 23:04
  */
 public class RocksDemo {
-    String dbPath = "/work/data/graph500";
-    RocksDB db;
 
-    @BeforeEach
-    public void bf() throws RocksDBException {
-        Options options = new Options();
-        options.setCreateIfMissing(true);
-        db = RocksDB.open(options,dbPath);
-    }
+    private static final String BASE_PATH = "/work/data/rocksdb/";
+    private static final String dbPath = BASE_PATH.concat("dh_test");
 
     @Test
     public void test() throws RocksDBException {
-        db.put("hello".getBytes(), "world".getBytes());
-        byte[] value = db.get("hello".getBytes());
-        System.out.println(new String(value));
+        RocksDBHelper.execute(dbPath, rocksDB -> {
+            try{
+                RocksDBHelper.put(rocksDB,"hello","world");
+                byte[] value = RocksDBHelper.get(rocksDB,"hello");
+
+//                rocksDB.put(ByteUtil.bytes("hello"), ByteUtil.bytes("world"));
+//                byte[] value = rocksDB.get(ByteUtil.bytes("hello"));
+                System.out.println(new String(value));
+            }catch (RocksDBException e){
+                e.printStackTrace();
+            }
+        });
     }
+
+
+    //  RocksDB.DEFAULT_COLUMN_FAMILY
+    @Test
+    public void testDefaultColumnFamily() throws RocksDBException {
+        RocksDBHelper.execute(dbPath,rocksDB -> {
+            try{
+                // 简单key-value
+                byte[] key = "Hello".getBytes();
+                rocksDB.put(key, "World".getBytes());
+
+                System.out.println(new String(rocksDB.get(key)));
+
+                rocksDB.put("SecondKey".getBytes(), "SecondValue".getBytes());
+
+                // 通过List做主键查询
+                List<byte[]> keys = Arrays.asList(key, "SecondKey".getBytes(), "missKey".getBytes());
+                List<byte[]> values = rocksDB.multiGetAsList(keys);
+                for (int i = 0; i < keys.size(); i++) {
+                    System.out.println("multiGet " + new String(keys.get(i)) + ":" + (values.get(i) != null ? new String(values.get(i)) : null));
+                }
+
+                // 打印全部[key - value]
+                RocksIterator iter = rocksDB.newIterator();
+                for (iter.seekToFirst(); iter.isValid(); iter.next()) {
+                    System.out.println("iterator key:" + new String(iter.key()) + ", iter value:" + new String(iter.value()));
+                }
+
+                // 删除一个key
+                rocksDB.delete(key);
+                System.out.println("after remove key:" + new String(key));
+
+                iter = rocksDB.newIterator();
+                for (iter.seekToFirst(); iter.isValid(); iter.next()) {
+                    System.out.println("iterator key:" + new String(iter.key()) + ", iter value:" + new String(iter.value()));
+                }
+            }catch (RocksDBException e){
+                e.printStackTrace();
+            }
+        });
+    }
+
+    // 使用特定的列族打开数据库，可以把列族理解为关系型数据库中的表(table)
+    @Test
+    public void testCertainColumnFamily() throws RocksDBException {
+        String cfName = "my-first-columnfamily";
+        RocksDBHelper.execute(dbPath,cfName,(rocksDB,cfHandles) -> {
+            try {
+                ColumnFamilyHandle cfHandle = cfHandles.stream().filter(x -> {
+                    try {
+                        return (new String(x.getName())).equals(cfName);
+                    } catch (RocksDBException e) {
+                        return false;
+                    }
+                }).collect(Collectors.toList()).get(0);
+
+                // 写入key/value
+                String key = "FirstKey";
+                rocksDB.put(cfHandle, key.getBytes(), "FirstValue".getBytes());
+                // 查询单key
+                byte[] getValue = rocksDB.get(cfHandle, key.getBytes());
+                System.out.println("get Value : " + new String(getValue));
+                // 写入第2个key/value
+                rocksDB.put(cfHandle, "SecondKey".getBytes(), "SecondValue".getBytes());
+
+                List<byte[]> keys = Arrays.asList(key.getBytes(), "SecondKey".getBytes());
+                List<ColumnFamilyHandle> cfHandleList = Arrays.asList(cfHandle, cfHandle);
+                // 查询多个key
+                List<byte[]> values = rocksDB.multiGetAsList(cfHandleList, keys);
+                for (int i = 0; i < keys.size(); i++) {
+                    System.out.println("multiGet:" + new String(keys.get(i)) + "--" + (values.get(i) == null ? null : new String(values.get(i))));
+                }
+
+                // 删除单key
+                rocksDB.delete(cfHandle, key.getBytes());
+
+                RocksIterator iter = rocksDB.newIterator(cfHandle);
+                for (iter.seekToFirst(); iter.isValid(); iter.next()) {
+                    System.out.println("iterator:" + new String(iter.key()) + ":" + new String(iter.value()));
+                }
+            } catch (RocksDBException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+
 }
